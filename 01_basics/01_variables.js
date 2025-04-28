6f440d372354c97dc1a3e179071451564fd4ec74
devSinghGit <115792746+devSinghGit@users.noreply.github.com>
2025-04-28 15:08:34 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 13552644;
+let accountName = "Satyam"
+var accountEmail = "satyam@google.com"
+accountPassword = "fcgd54crtdyd54"
+
+
+console.log(accountId);
+
+console.table([accountId, accountName, accountEmail, accountPassword])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treal all JS code in newer version
+
+// alert(1 + 4) // we are using nodeJs, not browser
+
+
+
+let name = "Satyam"
+let age = 25
+let isLoogedIn = false
+
+
+// number = 2 is to power 52
+// bigint = for a too much big number
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = no value defined
+// symbol = used to define unique type
+
+
+// object
+
+console.log(typeof name); // string
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
