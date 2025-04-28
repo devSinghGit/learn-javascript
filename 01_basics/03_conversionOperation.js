d256606ceafd661c155f60d0c1422ba7a0167680
devSinghGit <115792746+devSinghGit@users.noreply.github.com>
2025-04-28 16:21:46 +0000

    Comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
-let score = "34wsdw";
+let score = "34abc";
 
-console.log(typeof score);
+// console.log(typeof score);
 let valueInNumber = Number(score); // Chnages type to number from string
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33;
 // "33abc" => NaN
@@ -12,8 +12,51 @@ console.log(valueInNumber);
 
 let isLoggedIn = "Satyam";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "Satyam" => true
 // "" => false
+
+
+
+// ********************************** operations **********************************
+
+let value = 5;
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 is to power 3
+// console.log(2/3);
+// console.log(2%3);
+
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log("1" + (2 + 2)); // 14
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(3 + 4 * 5 % 7); // wrong way, always use () to priorities what to execute first
+// console.log((3 + 4) * 5 % 7);
+
+
+
+let x = 3;
+let y = x++; // If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+// console.log(x); // 4
+// console.log(y); // 3
+
+
+let a = 10;
+let b = ++a; // If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+// console.log(a); // 11
+// console.log(b); // 11
+
+// Source: If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// Always check typeof before using any comparision operator, JS changes it automatically. Output cam be different than expected
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
